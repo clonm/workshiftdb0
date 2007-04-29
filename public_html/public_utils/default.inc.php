7c5099727fc9b293fa75944ff100a1d07569787d
kanaj <kanaj>
2007-04-29 23:23:16 +0000

    Updated to be the same as the latest version already in public_html/ and admin/ -- more robust under directory structure changes

--- public_html/public_utils/default.inc.php
+++ public_html/public_utils/default.inc.php
@@ -1,15 +1,31 @@
 <?php
-
-$basedir = dirname($_SERVER['PATH_TRANSLATED']);
-$comps = explode('/',$basedir);
-while (count($comps) && $comps[count($comps)-1] !== 'workshift') {
-  array_pop($comps);
+//try to figure out where php_includes lives.  But really, you should just set 
+//$php_includes manually, unless you control PHP and can set the include_path
+$success = false;
+foreach (array('DOCUMENT_ROOT','PATH_TRANSLATED','SCRIPT_FILENAME','PWD') as $key) {
+  $success_basedir = false;
+  foreach (array('HTTP_SERVER_VARS','_SERVER','HTTP_ENV_VARS','_ENV') as $global) {
+    if (array_key_exists($key,$GLOBALS[$global]) && $GLOBALS[$global][$key]) {
+      $basedir = realpath(dirname($GLOBALS[$global][$key]));
+      break;
+    }
+    if (!$basedir) {
+      break;
+    }
+    $comps = explode('/',$basedir);
+    while (count($comps)) {
+      if (file_exists(implode('/',$comps) . '/php_includes')) {
+        $success = true;
+        break 2;
+      }
+      array_pop($comps);
+    }
+  }
 }
-if (!count($comps)) {
+if (!$success) {
   exit("Directory structure is not set up properly");
 }
-array_push($comps,'php_includes');
-$php_includes = implode('/',$comps);
+$php_includes = implode('/',$comps) . '/php_includes';
 if (!isset($delay_include)) {
   include_once("$php_includes/janakdb.inc.php");
 }
