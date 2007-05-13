5b7393d96fe540da4940169e95ad53c2929347d9
kanaj <kanaj>
2007-05-13 20:59:53 +0000

    Avoid stupid Forbidden errors, give users more index pages.

---
+++ public_html/elections/index.php
@@ -0,0 +1,12 @@
+<html><head><title>Elections Page</title></head><body>
+<?php
+require_once('default.inc.php');
+print_help();
+?>
+<ul>
+<li><a href='voting.php'>Vote in Elections</a>
+<li><a href='election_results.php'>View Election Results</a>
+<li><a href='elections_log.php'>View Elections Log (to make sure nothing fishy goes on)</a>
+<li><a href='../person.php'>Back to your personal page.</a>
+</ul>
+</body></html>
