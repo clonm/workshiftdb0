e2d19d3df0cf54dd01be1d73cdc61686d7725c66
kanaj <kanaj>
2007-05-17 09:02:40 +0000

    New default.inc.php model -- now just includes default.inc.php one level up.  Also, =workshift is set here, since it should really be set for all scripts in this directory (except update_house.php).

--- public_html/admin/default.inc.php
+++ public_html/admin/default.inc.php
@@ -1,32 +1,13 @@
 <?php
-//try to figure out where php_includes lives.  But really, you should just set 
-//$php_includes manually, unless you control PHP and can set the include_path
-$success = false;
-foreach (array('DOCUMENT_ROOT','PATH_TRANSLATED','SCRIPT_FILENAME','PWD') as $key) {
-  $success_basedir = false;
-  foreach (array('HTTP_SERVER_VARS','_SERVER','HTTP_ENV_VARS','_ENV') as $global) {
-    if (array_key_exists($key,$GLOBALS[$global]) && $GLOBALS[$global][$key]) {
-      $basedir = realpath(dirname($GLOBALS[$global][$key]));
-      break;
-    }
-    if (!$basedir) {
-      break;
-    }
-    $comps = explode('/',$basedir);
-    while (count($comps)) {
-      if (file_exists(implode('/',$comps) . '/php_includes')) {
-        $success = true;
-        break 2;
-      }
-      array_pop($comps);
-    }
-  }
+//just includes the higher-directory include file -- this is
+//appropriate for any tree structure, so long as we don't have any
+//directories out on their own.
+
+//require workshift privilege on all php files in this directory
+//unless they say otherwise
+if (!isset($require_user)) {
+  $require_user = 'workshift';
 }
-if (!$success) {
-  exit("Directory structure is not set up properly");
-}
-$php_includes = implode('/',$comps) . '/php_includes';
-if (!isset($delay_include)) {
-  include_once("$php_includes/janakdb.inc.php");
-}
+
+require_once('../default.inc.php');
 ?>
