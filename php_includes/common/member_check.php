8c1d39f33328fe4a695130bcd739d2a078ccb8f1
kanaj <kanaj>
2007-05-19 08:27:45 +0000

    Commented, fixed bug with hidden member_name input, wrote javascript to focus first field that should be focused.

--- php_includes/common/member_check.php
+++ php_includes/common/member_check.php
@@ -1,7 +1,12 @@
 <?php
+//generic login page.  Include()d by require_user().  stores all
+//values in submitted form in hidden inputs, has option to not display
+//password if we are in administrative context, will focus on first
+//field that "should" receive input.
 require_once('default.inc.php');
+//get rid of all cookies
 foreach ($_COOKIE as $key => $val) {
-  setcookie($key,stripformslash($val),time()-10800,"/");
+  setcookie($key,$val,time()-10800,"/");
   setcookie($key,"",0,"/");
   unset($_REQUEST[$key]);
 }
@@ -13,32 +18,44 @@ else {
   $member_name = null;
 }
 ?>
+
 <form action='<?=this_url()?>' method=POST>
-   Name: <select name='member_name'>
+   Name: <select name='member_name' id='member_name'>
 <OPTION>
 <?php
 foreach ($houselist as $name) {
   if ($name !== $member_name) {
-    print "<OPTION>" . escape_html($name) . "\n";
+    //remember, need to have value= in option to avoid problems with
+    //bad names
+    print "<OPTION value='" . escape_html($name) . 
+    "'>" . escape_html($name) . "\n";
   }
   else {
-    print "<OPTION selected>" . escape_html($name) . "\n";
+    print "<OPTION selected value='" . escape_html($name) . 
+    "'>" . escape_html($name) . "\n";
   }
 }
 ?>
 </SELECT>
     <?php 
+//did page that include()d tell us not to display password?
     if (!isset($skip_password) || !$skip_password) {
   ?>
-    Password: <input type=password name='passwd'>
+    Password: <input type=password name='passwd' id='passwd'>
                  <?php 
                  } 
     else { 
+      //no passwd id here, because never want to select it
 ?>
 <input type=hidden name='passwd' value=''>
 <?php
    }
+//put all remaining form variables in
 foreach ($_REQUEST as $key => $val) {
+  //except for the ones displayed above
+  if ($key == 'member_name' || $key == 'passwd') {
+    continue;
+  }
   if (!is_array($val)) {
     print "<input type=hidden name='" . escape_html($key) . "' " .
       "value='" . escape_html($val) ."' />\n";
@@ -54,3 +71,13 @@ foreach ($_REQUEST as $key => $val) {
 <input type=submit value='Submit'>
 </form>
 (You must have cookies enabled to log in)
+<script type='text/javascript'>
+//focus first field that should be focused
+if (document.getElementById('member_name').value &&
+    document.getElementById('passwd')) {
+      document.getElementById('passwd').focus();
+    }
+else {
+  document.getElementById('member_name').focus();
+}
+</script>
