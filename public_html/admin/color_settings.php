9f83c2f4ca704835e55819dcfd4978c7019ddf9e
kanaj <kanaj>
2007-05-19 01:22:17 +0000

    Deleted unused files.

--- public_html/admin/color_settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('default.inc.php');
-?>
-<html><head><title>Color Settings</title></head>
-<form action='<?=escape_html($_SERVER['REQUEST_URI'])?>' method=post>
-<input type=hidden name='submitting' value='1'>
-<?php
-if (array_key_exists('submitting',$_REQUEST)) {
-  $attribs = array('use_color','use_color_print')
