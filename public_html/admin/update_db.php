308cb289f8d53fd6b49305d2d83f8ee4d333fcb1
kanaj <kanaj>
2007-08-27 08:41:18 +0000

    Allowed house manager to edit fines.

--- public_html/admin/update_db.php
+++ public_html/admin/update_db.php
@@ -1,6 +1,9 @@
 <?php
 $delay_include = true;
 $body_insert = '';
+if ($_REQUEST['table_name'] == 'fining_data') {
+  $require_user = array('house', 'workshift');
+}
 require_once('default.inc.php');
 require_once("$php_includes/update_db.internal.php");
 ?>
