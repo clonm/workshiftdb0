78fed87f7bfa8831289f525ba26c13bfcbbd9315
kanaj <kanaj>
2008-02-02 04:35:58 +0000

    Fixed up output, warned user about size limitation.

--- public_html/admin/upload_workshift_doc.php
+++ public_html/admin/upload_workshift_doc.php
@@ -1,9 +1,11 @@
+<?php
+$body_insert = '';
+require_once('default.inc.php');
+?>
 <html><head><title>Upload Workshift Doc</title></head><body>
 <?php
-require_once('default.inc.php');
 print_help();
-print_r($_FILES['description']['tmp_name']);
-print "hi there";
+print $body_insert;
 if (!isset($_FILES['description']) || !isset($_FILES['description']['tmp_name'])
     || !strlen($_FILES['description']['tmp_name'])) {
   $row = $db->GetRow("select `filename` from `workshift_description`");
@@ -16,14 +18,12 @@ if (!isset($_FILES['description']) || !isset($_FILES['description']['tmp_name'])
    enctype="multipart/form-data">
 <input type="file" name="description" size="40">
 <input type=submit value='Upload Description'>
+   (Your file must be less than 1MB in size)
 </form>
 <?php
    exit;
 }
-exit("hi there\n");
-$db->debug = true;
 $file = file_get_contents($_FILES['description']['tmp_name']);
-$db->Execute("set global max_allowed_packet 160000000");
 if ($db->Execute("delete from `workshift_description`") &&
     $db->Execute("insert into `workshift_description` values (?,?)",
                  array($file,$_FILES['description']['name']))) {
