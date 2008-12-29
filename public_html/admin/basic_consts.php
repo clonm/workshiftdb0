1100a32d9953c4b31b498b423dd9c01d81326964
kanaj <kanaj>
2008-12-29 18:26:14 +0000

    Fixed get_static call bug.

--- public_html/admin/basic_consts.php
+++ public_html/admin/basic_consts.php
@@ -260,7 +260,7 @@ foreach ($_POST as $key => $val) {
 //If the total number of weeks is more than the week some fining period ends,
 //that fining period will become a "zombie," so we just clear it out.
 $db->Execute("delete from `fining_periods` " .
-             "where `week` > get_static('tot_weeks')");
+             "where `week` > ?", array(get_static('tot_weeks')));
 
 //not using ratings, but that just means we're rating out of 2, with default 1
 if (!$shift_flag) {
