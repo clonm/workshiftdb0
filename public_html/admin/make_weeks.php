045bcc797c8554f5270a7874cdb889cfc56b486d
kanaj <kanaj>
2011-09-02 13:21:40 +0000

    modernized make_weeks.php to use create_and_update_weekly_totals_data()

--- public_html/admin/make_weeks.php
+++ public_html/admin/make_weeks.php
@@ -4,7 +4,7 @@ if (!isset($_REQUEST['start_week'])) {
 ?>
 <html><head><title>Generate Weeks</title></head><body>
 <h2>You should no longer need to use this page.  Weeks will be generated automatically
-when you try to view them.  If you have questions, please email janak@berkeley.edu</h2>
+when you try to view them.  If you have questions, please email workshiftadmin@gmail.com</h2>
 <form action="<?=escape_html($_SERVER['REQUEST_URI'])?>" method=post>
 <table>
 <tr>
@@ -110,80 +110,9 @@ for ($ii = $start_week; $ii <= $end_week; $ii++) {
   if (!isset($suppress_output)) {
     echo "<a href='week.php?week=$ii'>Week $ii</a> successfully created!<p>";
   }
-  if ($USE_MYSQL_FEATURES) {
-    $db->Execute("GRANT SELECT ON " . bracket($tbl) . " TO ?@?",
-                 array($house_member,$member_loc));
-  }
 }
-$db->SetFetchMode(ADODB_FETCH_NUM); 
-$check_tables = $db->Execute("SHOW TABLES LIKE ?",
-  array(quote_mysqlreg($archive. "week_") . "%"));
-$db->SetFetchMode(ADODB_FETCH_ASSOC); 
-$week_tables = array();
-while ($tbl = $check_tables->FetchRow()) {
-  $ii = substr($tbl[0],5);
-  $week_tables[$ii] = 1;
-}
-//we need to create weekly_totals_data to be the largest it can be.  It's
-//possible that a previous run of this script had not created the largest
-//possible weekly_totals_data sheet because there were missing weeks which
-//this run has now filled in.
-for ($ii = 0; array_key_exists($ii,$week_tables) && $ii <= 100; $ii++) {}
-if ($ii === 100) {
-  trigger_error("There are way too many weekly tables for any good reason.",
-                E_USER_ERROR);
-}
-$final_week = $ii-1;
 
-//check to see if weekly_totals_data (which has the owed hours for each
-//person) exists
-if (!$db->Execute('CREATE TABLE if not exists' .
-  bracket($archive . 'weekly_totals_data') . ' (' .
-                  bracket('autoid') . 
-                  ' INT(11) AUTO_INCREMENT PRIMARY KEY, ' . 
-                  bracket('member_name') . ' VARCHAR(50) UNIQUE KEY, ' .
-                  bracket('notes') . ' LONGTEXT)')) {
-  trigger_error("Could not create table weekly_totals_data: " . $db->ErrorMsg(),
-                E_USER_ERROR);
-}
-if ($USE_MYSQL_FEATURES) {
-  $db->Execute("GRANT SELECT ON " . bracket('weekly_totals_data') . 
-               " TO ?@?",array($house_member,$member_loc));
-}
-//insert columns -- just laziness that they're not inserted above if it's a
-//new table, but code is shorter this way, although SQL is probably slower
-//how many of the right columns do we have?
-$check_data = $db->Execute('SHOW COLUMNS FROM ' . 
-                           bracket($archive . 'weekly_totals_data') . ' LIKE ' . 
-                           "'owed %'");
-//count how many columns we have
-$ii = 0;
-while ($row = $check_data->FetchRow()) {
-  $ii++;
-}
-//what should be inserted into each column?
-if (!isset($owed_default)) {
-  $owed_default = get_static('owed_default',5);
-}
-//for the remaining columns, insert them
-for (; $ii <= max($final_week,get_static('tot_weeks',18)); $ii++) {
-  //they get inserted after the previous one
-  if ($ii > 0) {
-    $db->Execute('ALTER TABLE ' . bracket($archive . 'weekly_totals_data') . ' ADD ' . 
-                 bracket("owed $ii") . ' FLOAT(4) NOT NULL DEFAULT ? AFTER ' .
-                 bracket('owed ' . ($ii-1)),array($owed_default));
-  }
-  //unless it's the first column, in which case they get inserted
-  //after the name
-  else {
-    $db->Execute('ALTER TABLE ' . bracket($archive . 'weekly_totals_data') . ' ADD ' . 
-		 bracket("owed 0") . ' FLOAT(4) NOT NULL DEFAULT ? AFTER ' . 
-                 bracket('member_name'),array($owed_default));
-  }
-  //insert default hours owed
-  $db->Execute('UPDATE ' . bracket($archive . 'weekly_totals_data') . ' SET ' . 
-               bracket("owed $ii") . ' = ?',array($owed_default));
-}
+create_and_update_weekly_totals_data();
 
 if (!isset($suppress_output)) {
   if ($start_week == $end_week) {
