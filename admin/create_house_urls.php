c32559c4c2bf39566e57abf734e67bbc935702e2
kanaj <kanaj>
2012-02-18 18:56:07 +0000

    Added more error reporting

--- admin/create_house_urls.php
+++ admin/create_house_urls.php
@@ -1,6 +1,7 @@
 <?php
 $houses = array('ath','aca','caz','clo','con','dav','euc','fen','hip','hoy',
 		'kid','kng','lot','nsc','rid','roc','she','stb','wil','wol','co');
+error_reporting((E_ALL | E_RECOVERABLE_ERROR) & ~E_STRICT);
 
 foreach ($houses as $house) {
   print "$house:<br>\n";
