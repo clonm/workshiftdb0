f658d145a92c9c663f020a5f1efab0bd827f120e
kanaj <kanaj>
2012-02-18 19:13:41 +0000

    made create_house_urls.php use standard path-finding scheme

--- admin/create_house_urls.php
+++ admin/create_house_urls.php
@@ -1,10 +1,10 @@
 <?php
 $delay_include = true;
 require_once('default.admin.inc.php');
-$docroot_dir = substr($php_includes,0,-strlen('/php_includes'));
+$docroot_dir = substr($php_includes,0,-strlen('php_includes'));
 
 print "Directory links will be created in the directory $docroot_dir<br/>\n";
 foreach ($houses as $house) {
   print "$house:<br>\n";
-  symlink($_SERVER['DOCUMENT_ROOT'] . '/public_html/',$_SERVER['DOCUMENT_ROOT'] . "/$house");
+  symlink($docroot_dir . 'public_html/',$docroot_dir . "$house");
 }
