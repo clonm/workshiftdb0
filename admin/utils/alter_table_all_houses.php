f7943e35793b6036bb0f4cec9945a6dc6829f243
kanaj <kanaj>
2012-05-20 03:27:39 +0000

    Modified default.inc.php to be correct

--- admin/utils/alter_table_all_houses.php
+++ admin/utils/alter_table_all_houses.php
@@ -4,9 +4,18 @@ janak_fatal_error_reporting(0);
 #$db->SetFetchMode(ADODB_FETCH_NUM);
 $db->debug = true; 
 foreach ($houses as $house) {
-  $db->Connect('localhost',"bsccoo5_wkshift","workshift","bsccoo5_workshift$house");
-  print "<h1>$house</h1>";
-  $db->Execute("ALTER TABLE `voting_record` add unique key " .
-  "(`member_name`,`election_name`)");
+ $db->Connect($url_array['server'],$url_array['user'],$url_array['pwd'],
+             $db_basename . $house);
+ print "<h1>$house</h1>";
+  $db->SetFetchMode(ADODB_FETCH_NUM);
+  $res = $db->Execute("show tables like '%master\_shifts'");
+  while ($row = $res->FetchRow()) {
+    $cols = $db->Execute("describe `" . $row[0] . "`");
+    $cols->FetchRow();
+    $cols->FetchRow();
+    $field = $cols->FetchRow();
+    if ($field[0] == 'floor') {
+      $db->Execute("alter table `" . $row[0] . "` drop column `floor`");
+    }
 }
-
+}
