4a30570753e20cb26565ae00f95bf0da0605a34f
kanaj <kanaj>
2012-09-01 13:00:48 +0000

    util

--- admin/utils/do_all_houses.php
+++ admin/utils/do_all_houses.php
@@ -5,10 +5,10 @@ janak_fatal_error_reporting(0);
 $db->debug = true;
 #$houses = array('nsc');
 foreach ($houses as $house) {
-  $db->Connect('localhost',"bsccoo5_wkshift","workshift","bsccoo5_workshift$house");
   print "<h1>$house</h1>";
-  $db->Execute("drop table old_wanted_shifts");
-  $db->Execute("rename table `wanted_shifts` to `old_wanted_shifts`, `new_wanted_shifts` to `wanted_shifts`");
+  $db->Connect($url_array['server'],$url_array['user'],$url_array['pwd'],
+    "$db_basename$house");
+  $db->Execute("alter table `GLOBAL_archive_data` add column `semester_end` bit(1) default NULL");
   /* $res = $db->Execute("select master_shifts.workshift, old_master_shifts.workshift from master_shifts left join old_master_shifts on master_shifts.autoid = old_master_shifts.autoid where master_shifts.workshift != old_master_shifts.workshift"); */
   /* while ($row = $res->FetchRow()) { */
   /*   print_r($row); */
